36b1ee29783792587349c9d484ec765e8d1d724c
Victor Vialard <victor.vialard@mines-paristech.fr>
2021-01-27 00:11:05 +0100

    Create graph file from md files using networkx

--- parse_roam.py
+++ parse_roam.py
@@ -4,58 +4,79 @@
 import argparse
 import os
 import re
-import glob
 import json
+import networkx as nx
+
+LINKS_SECTION_DELIMITER = "## Links to this note {#links-to-this-note}"
 
 
-def list_roam_files(roam_path):
-    return glob.glob(os.path.join(roam_path, "**/*.org"), recursive=True)
+def list_slugs(posts_path):
+    files = os.listdir(posts_path)
+    return [path[:-3] for path in files]
 
 
-def parse_links(files_list):
-    data = dict()
-    private_slugs = list()
-    i = 0
-    for filepath in files_list:
-        with open(filepath, "r") as f:
-            content = f.read()
+def read_file(filepath):
+    content = ""
+    with open(filepath, "r") as f:
+        for line in f.readlines():
+            if line.startswith(LINKS_SECTION_DELIMITER):
+                return content
+            content += line
+    return content
 
-            slug = re.findall("([^/]+)\.org$", filepath)[0]
-            roam_tags = re.findall("#\+ROAM_TAGS: ([a-z]+)", content) + re.findall(
-                "#\+roam_tags: ([a-z]+)", content
-            )
 
-            if "private" in roam_tags or "draft" in roam_tags:
-                private_slugs.append(slug)
+def extract_title(content):
+    title = re.findall('title = "(.+)"', content)[0]
+    return title
 
-            else:
-                title = (
-                    re.findall("#\+TITLE[: ]+(.+)[\n$]", content)
-                    + re.findall("#\+title[: ]+(.+)[\n$]", content)
-                )[0]
-                links = re.findall("\[\[file:([A-Za-z0-9_-]+)\.org\]", content)
 
-                data[slug] = {
-                    "id": i,
-                    "slug": slug,
-                    "title": title,
-                    "links": links,
-                }
-                i += 1
+def extract_links(content):
+    links = re.findall('<\s+relref\s+"([^"]+)"\s+>', content)
+    return links
 
-    for d in data.values():
-        d["links"] = [
-            data[slug]["id"] for slug in d["links"] if slug not in private_slugs
-        ]
 
-    return data
+def create_graph_from_slugs(posts_path, slugs_list):
+    graph = nx.DiGraph()
+    edges = list()
+
+    for index, slug in enumerate(slugs_list):
+
+        content = read_file(os.path.join(posts_path, slug + ".md"))
+
+        title = extract_title(content)
+
+        graph.add_node(index, id=index, title=title, slug=slug, rank=1)
+
+        links = extract_links(content)
+        for l in links:
+            edges.append((index, slugs_list.index(l)))
+
+    graph.add_edges_from(edges)
+
+    return graph
+
+
+def pagerank(graph):
+    for node, rank in nx.pagerank(graph, alpha=0.9).items():
+        graph.nodes[node]["rank"] = rank * 10
+
+
+def dump_graph(filepath):
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+    with open(filepath, "w") as out:
+        json.dump(
+            {
+                "nodes": [l[1] for l in list(graph.nodes(data=True))],
+                "links": [{"source": l[0], "target": l[1]} for l in graph.edges()],
+            },
+            out,
+        )
 
 
 def parse_cli():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-r", "--roam", help="Org Roam directory", required=True)
-    parser.add_argument("-u", "--hugo", help="Hugo website location", required=True)
+    parser.add_argument("--hugo", help="Hugo website location", required=True)
 
     return parser.parse_args()
 
@@ -63,24 +84,13 @@ def parse_cli():
 if __name__ == "__main__":
     args = parse_cli()
 
-    os.makedirs(os.path.join(args.hugo, "static/data/"), exist_ok=True)
+    posts_path = os.path.join(args.hugo, "content/posts/")
+    graph_path = os.path.join(args.hugo, "static/data", "graph.json")
 
-    files_list = list_roam_files(args.roam)
+    files_list = list_slugs(posts_path)
 
-    data = parse_links(files_list)
+    graph = create_graph_from_slugs(posts_path, files_list)
 
-    with open(os.path.join(args.hugo, "static/data/graph.json"), "w") as out:
-        json.dump(
-            {
-                "nodes": [
-                    {"id": l["id"], "title": l["title"], "slug": l["slug"], "rank": 1}
-                    for l in data.values()
-                ],
-                "links": [
-                    {"source": l["id"], "target": t}
-                    for l in data.values()
-                    for t in l["links"]
-                ],
-            },
-            out,
-        )
+    pagerank(graph)
+
+    dump_graph(graph_path)
